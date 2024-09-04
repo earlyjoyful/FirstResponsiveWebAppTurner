6041688e03281da1ea81ce64a15a97b84429c831
ejtur <ejtur@EARLY-VOSTRO>
2024-09-04 14:59:44 -0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using FirstResponsiveWebAppTurner.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstResponsiveWebAppTurner.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Birthday Calculator";
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(FirstResponsiveWebAppModel model)
+        {
+            ViewBag.BirthYear = model.AgeThisYear();
+            return View(model);
+        }
+    }
+}
---
+++ Models/FirstResponsiveWebAppModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.ComponentModel.DataAnnotations;
+
+namespace FirstResponsiveWebAppTurner.Models
+{
+    public class FirstResponsiveWebAppModel
+    {
+        [Required(ErrorMessage = "Please enter your name.")] 
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your 4-digit birth year.")]
+        [Range(1,2024, ErrorMessage = "Please enter a year between 1 and 2024.")] //unsure why this has double arrows for input/increment/decrement
+        public int? BirthYear { get; set; }
+
+        public const int THISYEAR = 2024;
+
+        public int? AgeThisYear()
+        {
+            return (THISYEAR - BirthYear);
+
+        }
+    }
+}
